ad3399844c8bf9aec8a88c26805f60e0aeacb91e
mikensothomas <thomasmikenson20@gmail.com>
2024-09-29 18:32:14 -0300

    Commit

--- janta.py
+++ janta.py
@@ -5,27 +5,29 @@ NUM_FILOSOFOS = 5
 tempo_de_comer = 4
 tempo_de_pensar = 4
 
-# A linha garfos = [threading.Semaphore(1) for _ in range(NUM_FILOSOFOS)] cria 5 garfos, um para cada filósofo.
 garfos = [threading.Semaphore(1) for _ in range(NUM_FILOSOFOS)]
-
 bloqueio = threading.Lock()
 
-grupo_comendo = [0, 1]
+grupo_comendo0 = [0, 2]
+grupo_comendo1 = [1, 3]
+grupo_comendo2 = [4, 2]
+
+grupo_comendo = grupo_comendo0
 
 def alternar_grupo():
     global grupo_comendo
 
-    if grupo_comendo == [0, 1]:
-        grupo_comendo = [2, 3]
-    elif grupo_comendo == [2, 3]:
-        grupo_comendo = [4, 0]
-    elif grupo_comendo == [4, 0]:
-        grupo_comendo = [1, 2]
+    if grupo_comendo == grupo_comendo0:
+        grupo_comendo = grupo_comendo1
+    elif grupo_comendo == grupo_comendo1:
+        grupo_comendo = grupo_comendo2
+    elif grupo_comendo == grupo_comendo2:
+        grupo_comendo = grupo_comendo0
 
 def filosofo(posicao):
     global grupo_comendo
     while True:
-        print(f"Filósofo {posicao} está pensando.")
+        # print(f"Filósofo {posicao} está pensando.")
         time.sleep(tempo_de_pensar)
 
         while True:
@@ -33,27 +35,20 @@ def filosofo(posicao):
                 if posicao in grupo_comendo:
                     break
 
-        print("....................................................................................................")
-        print("\n")
-
         garfo_esquerdo = garfos[posicao]
         garfo_direito = garfos[(posicao + 1) % NUM_FILOSOFOS]
         garfo_esquerdo.acquire()
         garfo_direito.acquire()
+
         print(f"Filósofo {posicao} pegou o garfo da esquerda (garfo {posicao}) e o garfo da direita (garfo {(posicao + 1) % NUM_FILOSOFOS})")
         print(f"Filósofo {posicao} está comendo.")
         time.sleep(tempo_de_comer)
 
-        print("....................................................................................................")
-        print("\n")
-
         print(f"Filósofo {posicao} terminou de comer e voltou a pensar.")
         garfo_esquerdo.release()
         garfo_direito.release()
-        print(f"O filósofo {posicao} largou o garfo (garfo {posicao}) e ( garfo {(posicao + 1) % NUM_FILOSOFOS})")
 
-        print("....................................................................................................")
-        print("\n")
+        print(f"Filósofo {posicao} largou o garfo da esquerda (garfo {posicao}) e da direita (garfo {(posicao + 1) % NUM_FILOSOFOS})")
 
         with bloqueio:
             alternar_grupo()
@@ -63,6 +58,7 @@ for i in range(NUM_FILOSOFOS):
     thread = threading.Thread(target=filosofo, args=(i,))
     threads.append(thread)
     thread.start()
+print("\n")
 
 for thread in threads:
     thread.join()
